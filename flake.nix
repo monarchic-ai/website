077743bfead86e807b8fe5af3a328c90d5988aab
omniscoder <omniscoder@users.noreply.github.com>
2026-07-07 18:52:53 +0000

    Add website Nix smoke shell

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Monarchic public website";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.chromium
+              pkgs.nodejs_22
+              pkgs.pnpm
+            ];
+
+            PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH = "${pkgs.chromium}/bin/chromium";
+          };
+        });
+    };
+}
